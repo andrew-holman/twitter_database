9296b123d1a9fa0330e4ed822ad38361c354abed
tmorvant <tmorvant@iastate.edu>
2020-11-17 17:35:26 -0600

    fixed all queries

--- thomas_queries.sql
+++ thomas_queries.sql
@@ -1,12 +1,14 @@
 -- Q7 where hashtag = "GOP", state = "NC", month = 10, year = 2016, k = 5
-select distinct count(twt.tid), twitter_user.screen_name, twitter_user.category
+select count(twt.tid) as tweets, twitter_user.screen_name, twitter_user.category
 from twitter_user
 join tweet as twt on twt.posting_user = twitter_user.screen_name
 join tweet_tag as tag on tag.tid = twt.tid
 where tag.tag_name = "GOPDebate"
-and twitter_user.state = "NC"
-and month(twt.posted) = 10
-and month(twt.posted) = 2016
+and twitter_user.state = "North Carolina"
+and month(twt.posted) = 2
+and year(twt.posted) = 2016
+group by twitter_user.screen_name
+order by count(twt.tid) desc
 limit 5;
 
 -- Q16 where k = 5, month = 2, year = 2016
@@ -21,12 +23,13 @@ order by twt.retweet_count desc
 limit 5;
 
 -- Q23 where k = 5, subcategroy = "GOP", month = [1,2,3], year = 2016
-select hashtag.tag_name, count(twt.tid) as num_uses
+select hashtag.tag_name, count(distinct twt.tid) as num_uses
 from tweet_tag as hashtag
 join tweet as twt on twt.tid = hashtag.tid
 join twitter_user as users on users.screen_name = twt.posting_user
 where users.subcategory = "GOP"
 and year(twt.posted) = 2016
 and month(twt.posted) between 1 and 3
-order by count(twt.tid) desc
+group by hashtag.tag_name
+order by count(distinct twt.tid) desc
 limit 5;
